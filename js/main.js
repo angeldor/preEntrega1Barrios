9cc0305b9e9e320666b55c1a7dfa74233ed986ce
AngelBarrios <angelefrenbarriosromero234@gmail.com>
2023-07-28 11:50:54 -0300

    primer vistazo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-let nombre = (prompt("Ingrese un nombre: "))
-let apellido = (prompt("Ingrese un apellido"))
-function saludarConNombre(){
-    console.log()
+let usuario = angel_barrios
+let password = hola123
+let usuarioIngresado = prompt("Ingresa tu nombre de usuario")
+
+if(usuarioIngresado === usuario){
+    let passwordIngresada = prompt("Ingresa tu contraseña")
+
+    if(passwordIngresada === password){
+        console.log("Bienvenido ${usuario} ")
+    }
+} else {
+    console.log("Usuario no encontrado, Intentelo de nuevo")
 }
